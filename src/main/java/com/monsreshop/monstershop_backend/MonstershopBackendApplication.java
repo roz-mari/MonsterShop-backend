8e3c1b7b12dc2ea05c1dd58c432f58142cd9dd71
rozmarymethod@gmail.com <rozmarymethod@gmail.com>
2025-06-16 12:15:13 +0200

    First commit

---
+++ src/main/java/com/monsreshop/monstershop_backend/MonstershopBackendApplication.java
@@ -0,0 +1,13 @@
+package com.monsreshop.monstershop_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonstershopBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonstershopBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/monsreshop/monstershop_backend/MonstershopBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monsreshop.monstershop_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonstershopBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
